cb7ccc3fe1b83e84f4eb51b7a7ae1169c6eddd18
veggiepilot <johanneschitura@gmail.com>
2021-08-17 19:29:55 -0400

    renderTodos

--- script.js
+++ script.js
@@ -3,28 +3,29 @@ var newTodoForm  = document.getElementById('new-todo-form');
 var newTodoInput = document.getElementById('new-todo');
 var todoList     = document.getElementById('todos-list');
 
-var todos = ["test", "test3333", "test3333walk the dog"];
+var todos = [];
 
-// loop through todos
-
-for (var i = 0; i < todos.length; i++) {
-    // create an LI 
-    var li = document.createElement("li");
-    // insert text in LI
-    li.innerText = todos[i];
-    // append element into todoList
-    todoList.appendChild(li);
-    
+function renderTodos() {
+    // clear out list in DOM
+    todoList.innerHTML = '';
+    // loop through todos
+    for (var i = 0; i < todos.length; i++) {
+        // create an LI 
+        var li = document.createElement("li");
+        // insert text in LI
+        li.innerText = todos[i];
+        // append element into todoList
+        todoList.appendChild(li);   
+    }
 }
-
-
 
 function addTodo(event) {
     event.preventDefault();
     var newTodoText = newTodoInput.value;
     todos.push(newTodoText);
     newTodoInput.value = '';
-    console.log(todos);
+    renderTodos();
 
 }
 newTodoForm.addEventListener('submit', addTodo);
+renderTodos();
