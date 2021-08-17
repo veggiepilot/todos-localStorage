c73d94a5704f7e1d64916110489eb3a02e8f1fb4
veggiepilot <johanneschitura@gmail.com>
2021-08-17 06:52:32 -0400

    html skeleton

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Yo');
+// localStorage.setItem('myName', 'Johannes');
+// localStorage.setItem('petName', 'Lucy');
+// localStorage.setItem('petAge', '9 months');
+
+// localStorage.setItem('petAge', 1);
+
+let myName = localStorage.getItem('myName');
+let petName = localStorage.getItem('petName');
+let petAge = localStorage.getItem('petAge');
+
+console.log(myName, petAge, petName);
