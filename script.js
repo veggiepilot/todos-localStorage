b021e8ace7d66ebc5cdc8837f71270fa85d0d946
veggiepilot <johanneschitura@gmail.com>
2021-08-17 19:57:30 -0400

    add delete button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 // select DOM elements
 var newTodoForm  = document.getElementById('new-todo-form');
 var newTodoInput = document.getElementById('new-todo');
@@ -14,6 +16,13 @@ function renderTodos() {
         var li = document.createElement("li");
         // insert text in LI
         li.innerText = todos[i];
+        // create button el
+        var button = document.createElement('button');
+        // add checkamark &#10004 to button
+        button.innerHTML = '&#10004;'
+        // append button into li 
+        li.appendChild(button);
+
         // append element into todoList
         todoList.appendChild(li);   
     }
