afbb2c10b5ce6f135db79a66acc5d90a71ead899
veggiepilot <johanneschitura@gmail.com>
2021-08-17 19:10:20 -0400

    render todos into DOM

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ var todoList     = document.getElementById('todos-list');
 
 var todos = ["test", "test3333", "test3333walk the dog"];
 
+// loop through todos
+
+for (var i = 0; i < todos.length; i++) {
+    // create an LI 
+    var li = document.createElement("li");
+    // insert text in LI
+    li.innerText = todos[i];
+    // append element into todoList
+    todoList.appendChild(li);
+    
+}
+
+
+
 function addTodo(event) {
     event.preventDefault();
     var newTodoText = newTodoInput.value;
